15d3d1eeb4f4254722ce76035a40de361721035f
Manuel Astonitas <yajhura342@gmail.com>
2023-10-04 23:03:40 -0500

    feat:new empleado

---
+++ ProyectoComanda/ViewControllers/EmpleadoDetalleViewControler.swift
@@ -0,0 +1,26 @@
+//
+//  EmpleadoDetalleViewControler.swift
+//  ProyectoComanda
+//
+//  Created by Yajhura on 4/10/23.
+//
+
+import UIKit
+
+class EmpleadoDetalleViewControler: UIViewController {
+    @IBOutlet weak var txtNombre: UITextField!
+    
+    @IBOutlet weak var txtTelefono: UITextField!
+    @IBOutlet weak var txtCorreo: UITextField!
+    @IBOutlet weak var txtDni: UITextField!
+    @IBOutlet weak var txtApellido: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
