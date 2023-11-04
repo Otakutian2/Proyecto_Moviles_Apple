f8e0de2add6916521126fdee9979fbbe67205068
sebastiancarajal <bembosyou2@gmail.com>
2023-11-03 23:53:37 -0700

    Agregando funcionalidad de los botones de Configuracion y Creando Vista Comanda

--- ProyectoComanda/ViewControllers/ConfiguracionViewController.swift
+++ ProyectoComanda/ViewControllers/ConfiguracionViewController.swift
@@ -15,13 +15,35 @@ class ConfiguracionViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    @IBAction func btnVistaCategoria(_ sender: Any) {
-        performSegue(withIdentifier: "vistaCategoria", sender: self)
-
+    @IBAction func vistaEmpleado(_ sender: Any) {
+        performSegue(withIdentifier: "vistaEmpleado", sender: self)
     }
+    @IBAction func vistaCategoria(_ sender: Any) {
+        performSegue(withIdentifier: "vistaCategoria", sender: self)
+    }
+    
+    @IBAction func vistaPlato(_ sender: Any) {
+        performSegue(withIdentifier: "vistaPlato", sender: self)
+    }
+    
+    @IBAction func vistaMesas(_ sender: Any) {
+        performSegue(withIdentifier: "vistaMesa", sender: self)
+        
+    }
+    
+    @IBAction func vistaEstablecimiento(_ sender: Any) {
+        performSegue(withIdentifier: "vistaEstablecimiento", sender: self)
+    }
+    
+    
+    @IBAction func vistaMetodoPago(_ sender: Any) {
+        performSegue(withIdentifier: "vistaMetodoPago", sender: self)
+    }
+    	
+    
     /*
-    // MARK: - Navigation
+    
+     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
